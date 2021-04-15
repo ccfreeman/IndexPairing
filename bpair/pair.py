f9b8b1c2b2bed717df08e72df5842e8c917e2c4a
Cole <cole.freeman@coyote.com>
2021-04-14 23:09:45 -0500

    Added pairing funcs and tests

--- bpair/pair.py
+++ bpair/pair.py
@@ -1,11 +1,32 @@
-import numpy as numpy
+import numpy as np
+
+N_BITSHIFT = 32
 
 def pair(a, b):
     """
+    Parameters
+    ----------
+    a : np.array of uint32
+    b : np.array of uint32
+    
+    Returns
+    -------
+    z : np.array of uint64
     """
-    pass
+    return (a.astype(np.uint64) << N_BITSHIFT) + b.astype(np.uint64)
+
 
 def unpair(z):
     """
+    Parameters
+    ----------
+    z : np.array of uint64
+    
+    Returns
+    -------
+    a : np.array of uint32
+    b : np.array of uint32
     """
-    pass
+    a = (z >> N_BITSHIFT).astype(np.uint32)
+    b = z.astype(np.uint32)
+    return a, b
--- tests/test_bpair.py
+++ tests/test_bpair.py
@@ -1,9 +1,17 @@
 import bpair
+import numpy as np
 
 
 def test_pair():
-    assert bpair.pair(4,5) == 0
+    a = np.array([1, 3], dtype=np.uint32)
+    b = np.array([15, 7], dtype=np.uint32)
+    actual = bpair.pair(a, b)
+    expected = np.array([4294967311, 12884901895], dtype=np.uint64)
+    assert all([x == y for x, y in zip(actual, expected)])
 
 
 def test_unpair():
-    assert bpair.unpair(5) == 0
+    expected_a = np.array([1, 3], dtype=np.uint32)
+    expected_b = np.array([15, 7], dtype=np.uint32)
+    actual_a, actual_b = bpair.unpair(np.array([4294967311, 12884901895], dtype=np.uint64))
+    assert all([x == y for x,y in zip(actual_a, expected_a)]) and all([x == y for x,y in zip(actual_b, expected_b)])
